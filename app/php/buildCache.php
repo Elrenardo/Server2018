f76450d2a7e4babc5cf1a8c6a8634e70031c9332
Guillaume Teysseire <teysseire@studiogoupil.fr>
2018-10-03 19:56:33 +0200

    add src file module
    gestion des fichiers SRC dans le compilateur

--- app/php/buildCache.php
+++ app/php/buildCache.php
@@ -3,21 +3,35 @@ include 'path.php';
 
 //suppresion cache
 echo 'Suppression cache ! <br/><br/>';
+//tout supprimé
+rrmdir($PATH_TMP.'src');
 rrmdir($PATH_TMP);
+
+//cration dossier cache
 mkdir($PATH_TMP);
+//création SRC
+mkdir($PATH_TMP.'src');
 
 
-echo 'Build cache module: <br/>';
-foreach (glob_recursive($PATH_FIND."/bundle/*.php") as $filename)
+echo '## Build cache module: <br/>';
+foreach (glob_recursive($PATH_FIND."/bundle/*/module/*.php") as $filename)
 {
 	echo 'Add module => '.basename($filename).'<br/>';
 	copy($filename,  $PATH_TMP.basename($filename));
 }
 
 
-echo '<br/><br/>Build cache route: <br/>';
-foreach (glob_recursive($PATH_FIND."*.route") as $filename)
+echo '<br/><br/>## Build cache route: <br/>';
+foreach (glob_recursive($PATH_FIND."/bundle/*/route/*.route") as $filename)
 {
 	echo 'Add route => '.basename($filename).'<br/>';
 	copy($filename,  $PATH_TMP.basename($filename));
+}
+
+
+echo '<br/><br/>## Build src cache: <br/>';
+foreach (glob_recursive($PATH_FIND."/bundle/*/src/*.php") as $filename)
+{
+	echo 'Add src => '.basename($filename).'<br/>';
+	copy($filename,  $PATH_TMP.'src/'.basename($filename));
 }
