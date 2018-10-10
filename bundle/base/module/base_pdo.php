87e79d5eedaf886b4579a510220647103aed3549
Guillaume Teysseire <teysseire@studiogoupil.fr>
2018-10-10 19:12:48 +0200

    patch base_pdo

--- bundle/base/module/base_pdo.php
+++ bundle/base/module/base_pdo.php
@@ -4,29 +4,54 @@ require 'vendor/autoload.php';
 class base_pdo extends SRVG\Module
 {
 
-	static $pdo = null;
+	private static $pdo = null;
 
 	//Start fonction
 	public function Start( $route, $config, $next )
+	{	
+		//ok
+		$next->next();
+	}
+
+
+	public static function get()
 	{
+		return $this->pdo;
+	}
+}
 
-		$bdd  = 'contain_paranormal';
-		$user = 'root';
-		$pass = '';
 
+//Class Auto Gestion BDD
+class PdoBdd
+{
+	static $datatable = 'contain_paranormal';
+	static $user      = 'root';
+	static $pass      = '';
+
+	//storage BDD
+	private static $bdd = null;
+
+	//connection
+	private static function connect()
+	{
 		//BDD connection
 		try {
-		    $dbh = new PDO('mysql:host=localhost;dbname='.$bdd, $user, $pass);
+		    $dbh = new PDO('mysql:host=localhost;dbname='.PdoBdd::$datatable, PdoBdd::$user, PdoBdd::$pass);
 		} catch (PDOException $e) {
 		    print "Erreur !: " . $e->getMessage() . "<br/>";
 		    die();
 		}
 
-		//save BDD
-		base_pdo::$pdo = $dbh;
-		$route->addData('pdo',$dbh);
-				
-		//ok
-		$next->next();
+		//save
+		PdoBdd::$bdd = $dbh;
+	}
+
+
+	//get BDD
+	public static function get()
+	{
+		if(is_null(PdoBdd::$bdd))
+			PdoBdd::connect();
+		return PdoBdd::$bdd;
 	}
 }
